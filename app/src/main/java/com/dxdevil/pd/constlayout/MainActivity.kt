28abd4994aabc933caa73031f65724415f38d023
Parashar Dhamecha <parashardhamecha913@gmail.com>
2019-02-21 14:58:46 +0530

    5th commit-[Kotlin file onclick event]

--- app/src/main/java/com/dxdevil/pd/constlayout/MainActivity.kt
+++ app/src/main/java/com/dxdevil/pd/constlayout/MainActivity.kt
@@ -2,11 +2,32 @@ package com.dxdevil.pd.constlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bt_juhi.setOnClickListener {
+            Toast.makeText(applicationContext,"Hi juhi",Toast.LENGTH_SHORT).show()
+        }
+
+
+        bt_rishabh.setOnClickListener {
+            Toast.makeText(applicationContext,"Hi Rishabh",Toast.LENGTH_SHORT).show()
+        }
+
+
+        bt_pratik.setOnClickListener {
+            Toast.makeText(applicationContext,"Hi Pratik",Toast.LENGTH_SHORT).show()
+        }
+
+
+        bt_parashar.setOnClickListener {
+            Toast.makeText(applicationContext,"Hi Parashar",Toast.LENGTH_SHORT).show()
+        }
     }
 }
